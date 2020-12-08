1b2cc7a294a16b03c17dcaa53c190d6fb1d907c3
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-08 22:50:13 +1000

    Created the base dealer class along with a singleton/instance

---
+++ Assets/Scripts/Dealer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dealer : MonoBehaviour
+{
+    public static Dealer Instance;
+
+    [Header("Card List")]
+    [SerializeField] List<ScriptableObject> Deck = new List<ScriptableObject>();
+    [SerializeField] List<ScriptableObject> Played = new List<ScriptableObject>();
+
+    public void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
