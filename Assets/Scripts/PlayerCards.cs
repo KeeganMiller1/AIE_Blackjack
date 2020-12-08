5f5528af076524f6b6ff8905c248509d17b64f12
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-08 22:58:11 +1000

    Added Positioning variables & Prefab

--- Assets/Scripts/PlayerCards.cs
+++ Assets/Scripts/PlayerCards.cs
@@ -6,6 +6,16 @@ public class PlayerCardController : MonoBehaviour
 {
     List<ScriptableObject> CardsInHand = new List<ScriptableObject>();
 
+    [Header("Prefabs")]
+    [SerializeField] GameObject CardPrefab;
+
+    [Header("Card Position")]
+    [SerializeField, Tooltip("Start position for the first card")] 
+    Vector2 StartPosition;
+    Vector2 LastUsedPosition;
+    [SerializeField, Tooltip("How much the X Position will increase each card")]
+    float CardPositionIncrement;
+
     void Awake()
     {
 
@@ -13,7 +23,11 @@ public class PlayerCardController : MonoBehaviour
 
     List<ScriptableObject> GetCardsInHand() => CardsInHand;
 
-    public void AddCard(ScriptableObject card) => CardsInHand.Add(card);
+    public void AddCard(ScriptableObject card)
+    {
+        CardsInHand.Add(card);
+        
+    }
     public void EmptyHand() => CardsInHand.Clear();
 
 }
