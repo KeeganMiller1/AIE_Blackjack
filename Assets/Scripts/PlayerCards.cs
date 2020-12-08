cddbb965bb057746bebc61f0f9f67ebae8419bd5
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-08 22:51:48 +1000

    Added Method to add a card & a method to clear the hand

---
+++ Assets/Scripts/PlayerCards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCardController : MonoBehaviour
+{
+    List<ScriptableObject> CardsInHand = new List<ScriptableObject>();
+
+    void Awake()
+    {
+
+    }
+
+    List<ScriptableObject> GetCardsInHand() => CardsInHand;
+
+    public void AddCard(ScriptableObject card) => CardsInHand.Add(card);
+    public void EmptyHand() => CardsInHand.Clear();
+
+}
