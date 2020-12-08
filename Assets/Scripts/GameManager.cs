bbb34ae93ffed9a2e1d0077d4528fc59578d8b7e
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-08 22:50:26 +1000

    Created the base GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] int TotalPlayers;
+    int PlayersTurn = 1;
+
+    void Awake()
+    {
+        // Create a singleton
+        if(Instance = null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
