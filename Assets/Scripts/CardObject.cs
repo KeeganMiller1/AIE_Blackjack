44a7d8c749d5b17914eddd897bfb3ca7ff4e7f3f
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-08 22:49:50 +1000

    Implemented the base Card Object

---
+++ Assets/Scripts/CardObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ECardSuite
+{
+    SPADE,
+    CLUB,
+    HEART,
+    DIAMOND
+}
+
+public enum ECardType
+{
+    STANDARD,
+    ACE,
+    JACK,
+    QUEEN,
+    KING
+}
+
+
+
+class CardObject : ScriptableObject 
+{
+    public ECardSuite CardSuit;
+    public int CardValue;
+    public ECardType CardType;
+
+    public Sprite CardSprite;
+}
+
