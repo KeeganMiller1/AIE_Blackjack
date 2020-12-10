fa8b14b9aaf4fc49bddd9b565772eec85a436c4e
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-10 23:44:52 +1000

    Renamed

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerCardController PlayerCards;
+
+    void Awake()
+    {
+        PlayerCards = GetComponent<PlayerCardController>();
+    }
+
+    PlayerCardController GetPlayerCards() => PlayerCards;
+
+}
