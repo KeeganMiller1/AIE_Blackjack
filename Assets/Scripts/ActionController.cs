17a9eab080dfe38bc741e42aced2b9e231d9326c
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-11 01:45:12 +1000

    Implemented a HIt Method

---
+++ Assets/Scripts/ActionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour
+{
+    public void Hit()
+    {
+        List<GameObject> Players = GameManager.Instance.GetPlayersInGame();
+
+        foreach(var p in Players)
+        {
+            PlayerController controller = p.GetComponent<PlayerController>();
+            if(controller != null)
+            {
+                if(controller.IsThisPlayer())
+                {
+                    if(controller.GetPlayerNum() == GameManager.Instance.GetPlayerTurn())
+                    {
+                        Dealer.Instance.DealCard(p.GetComponent<PlayerCardController>());
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
