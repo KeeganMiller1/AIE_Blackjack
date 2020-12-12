87f9d9ef6f108cd60f7dd9de92193dff69f5bd42
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-12 21:25:38 +1000

    Added Betting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,14 +10,53 @@ public class PlayerController : MonoBehaviour
 
     bool IsPlayer = true;
 
+    int TotalChips = 100;
+    int CurrentBet = 0;
+
     void Awake()
     {
         PlayerCards = GetComponent<PlayerCardController>();
     }
 
+    void Start()
+    {
+        UpdateBetValue();
+    }
+    
+
     PlayerCardController GetPlayerCards() => PlayerCards;
     public void SetPlayerNum(int num) => PlayerGameNumber = num;
     public int GetPlayerNum() => PlayerGameNumber;
     public bool IsThisPlayer() => IsPlayer;
+
+    public void AddBet(int BetAmount)
+    {
+        if (BetAmount <= (TotalChips - CurrentBet))
+        {
+            CurrentBet += BetAmount;
+            UpdateBetValue();
+        } else
+        {
+            Debug.Log("Not Enough Chips");
+        }
+    }
+
+    public void ConfirmBet()
+    {
+        Dealer.Instance.AddToPot(CurrentBet);
+        CurrentBet = 0;
+        UpdateBetValue();
+    }
+
+
+    void UpdateBetValue()
+    {
+        // Set the bet details
+        var go = GameObject.FindGameObjectWithTag("Bet Text");
+        go.GetComponent<Text>().text = "BET: $" + CurrentBet;
+        // Set the new balance of the pot
+        var currentValue = GameObject.FindGameObjectWithTag("Player Chips");
+        currentValue.GetComponent<Text>().text = TotalChips.ToString();
+    }
 
 }
